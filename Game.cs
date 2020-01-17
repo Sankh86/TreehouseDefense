d3d2930620ada1aedd4f8bba9ec8c3ae6bdf5551
David Cheatham <sancrock@gmail.com>
2020-01-17 17:13:24 -0500

    finished methods tutorial

--- Game.cs
+++ Game.cs
@@ -1,12 +1,20 @@
-﻿namespace TreehouseDefense
+﻿using System;
+
+namespace TreehouseDefense
 {
     class Game
     {
         public static void Main()
         {
         	Map map = new Map(8,5);
+            Point point = new Point(4,2);
 
-			int area = map.Width * map.Height;
+            bool isOnMap = map.OnMap(point);
+            Console.WriteLine(isOnMap);
+
+            point = new Point (8, 5);
+            isOnMap = map.OnMap(point);
+            Console.WriteLine(isOnMap);
 		}
     }
 }
--- Point.cs
+++ Point.cs
@@ -1,14 +1,25 @@
+using System;
+
 namespace TreehouseDefense
 {
-	class Point
-	{
-		public readonly int X;
-		public readonly int Y;
+    class Point
+    {
+        public readonly int X;
+        public readonly int Y;
 
-		public Point(int x, int y)
-		{
-			X = x;
-			Y = y;
-		}
-	}
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int DistanceTo(int x, int y)
+        {
+            return (int)Math.Sqrt(Math.Pow(X - x, 2) + Math.Pow(Y - y, 2));
+        }
+
+        public int DistanceTo(Point point){
+            return DistanceTo(point.X, point.Y);
+        }
+    }
 }
