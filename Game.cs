ef69792511152ce5d5de5a4af102671cf5835414
David Cheatham <sancrock@gmail.com>
2020-01-20 16:30:19 -0500

    Finished Encapsulation & Arrays

--- Game.cs
+++ Game.cs
@@ -6,15 +6,43 @@ namespace TreehouseDefense
     {
         public static void Main()
         {
-        	Map map = new Map(8,5);
-            Point point = new Point(4,2);
+            Map map = new Map(8, 5);
 
-            bool isOnMap = map.OnMap(point);
-            Console.WriteLine(isOnMap);
+            try
+            {
+                Path path = new Path(
+                    new[] {
+                        new MapLocation(0, 2, map),
+                        new MapLocation(1, 2, map),
+                        new MapLocation(2, 2, map),
+                        new MapLocation(3, 2, map),
+                        new MapLocation(4, 2, map),
+                        new MapLocation(5, 2, map),
+                        new MapLocation(6, 2, map),
+                        new MapLocation(7, 2, map)
+                    }
+                );
 
-            point = new Point (8, 5);
-            isOnMap = map.OnMap(point);
-            Console.WriteLine(isOnMap);
-		}
+                MapLocation location = path.GetLocationAt(8);
+                Console.WriteLine($"{location.X},{location.Y}");
+            }
+            catch (OutOfBoundsException ex)
+            {
+                Console.Write(ex.Message);
+            }
+            catch (TreehouseDefenseException)
+            {
+                Console.Write("Unhandled TreehouseDefenseException");
+            }
+            catch (Exception ex)
+            {
+                Console.Write($"Unhandled Exception: {ex}");
+            }
+
+            // bool isOnMap = map.OnMap(x);
+            // Console.WriteLine(isOnMap);
+
+            // Console.WriteLine(x.DistanceTo(5, 5));
+        }
     }
 }
